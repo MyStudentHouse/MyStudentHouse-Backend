3c66e1479ea470aefa4fe1de93a4b69ee923d9bf
Patrick van Broeckhuijsen <pvanbroeckhuijsen+github@gmail.com>
2019-01-27 20:25:13 +0100

    Fixed user authentication for BeerController.

--- routes/api.php
+++ routes/api.php
@@ -13,14 +13,18 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::redirect('/login', '/');
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('beer', 'API\BeerController@show')->middleware('cors');
-Route::post('beer', 'API\BeerController@store')->middleware('cors');
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::get('beer', 'API\BeerController@show')->middleware('cors');
+    Route::post('beer', 'API\BeerController@store')->middleware('cors');
+});
 
-Route::post('login', 'API\UserController@login')->middleware('cors');
+Route::post('login', ['as' => 'login', 'uses' => 'API\UserController@login'])->middleware('cors');
 Route::post('register', 'API\UserController@register')->middleware('cors');
 Route::group(['middleware' => 'auth:api'], function(){
     Route::post('details', 'API\UserController@details')->middleware('cors');
