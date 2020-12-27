786173c85ef88790ed1670325489fa8300afd7bd
Patrick van Broeckhuijsen <github@patb.ro>
2020-12-27 17:33:48 +0100

    Add base functionality for sending email to user assigned to house

---
+++ resources/views/emails/house.useradded.blade.php
@@ -0,0 +1,9 @@
+@if (empty($name))
+    Hi there!
+@else
+    Hey {{name}}!
+@endif
+
+Just now, {{nameAdded}} added you to {{houseName}} on My Student House.
+
+Create an account or login on https://mystudent.house/
