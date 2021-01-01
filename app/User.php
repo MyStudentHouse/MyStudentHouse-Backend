26f9e6316a20fdb055bedf0be61c6c4f0846163b
Patrick van Broeckhuijsen <github@patb.ro>
2021-01-01 14:03:42 +0100

    Add SoftDeletes to User Model

--- app/User.php
+++ app/User.php
@@ -11,7 +11,7 @@ use App\Notifications\VerifyEmailNotification;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
-    use Notifiable, HasApiTokens;
+    use Notifiable, HasApiTokens, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
